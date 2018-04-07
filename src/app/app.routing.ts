2b2a9e4a43dd12ff1660dd10c22f664afa39171f
Tarcísio Th <martins_tarcisio@bitbucket.org>
2018-04-07 16:20:43 -0300

    clean

--- src/app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import {DatepickerSampleComponent} from './datepicker-sample/datepicker-sample.component';
-import {ModuleWithProviders} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
-const APP_ROUTES: Routes = [
-    { path: '**', component: DatepickerSampleComponent }
-];
-export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
--- src/app/month-picker/month.ts
+++
@@ -1,4 +0,0 @@
-interface Month {  
-     val: string;
-     name: string;
-}
